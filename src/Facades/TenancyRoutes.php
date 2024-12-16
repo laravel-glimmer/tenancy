97636df77e8a1dfbf7d41b8f983a29c54769461b
Haruki1707 <72423267+Haruki1707@users.noreply.github.com>
2024-12-16 17:50:40 -0600

    Add missing parameters to TenancyRoutes

--- src/Facades/TenancyRoutes.php
+++ src/Facades/TenancyRoutes.php
@@ -8,8 +8,8 @@ use Illuminate\Routing\RouteRegistrar;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static RouteRegistrar landlord(array|Closure|string|null $routes = null)
- * @method static RouteRegistrar tenant(array|Closure|string|null $routes = null)
+ * @method static RouteRegistrar landlord(array|Closure|string|null $routes = null, string $namePrefix = 'landlord.', string|array|null $extraMiddlewares = null)
+ * @method static RouteRegistrar tenant(array|Closure|string|null $routes = null, string $namePrefix = 'tenant.', string|array|null $extraMiddlewares = null)
  */
 class TenancyRoutes extends Facade
 {
