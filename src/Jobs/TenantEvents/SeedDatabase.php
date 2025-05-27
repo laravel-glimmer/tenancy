22b10aa647053e950c1e2a472657fa2b5a839b09
Haruki1707 <72423267+Haruki1707@users.noreply.github.com>
2025-05-27 08:08:42 -0600

    Add custom seeder option to SeedDatabase tenant event queue

--- src/Jobs/TenantEvents/SeedDatabase.php
+++ src/Jobs/TenantEvents/SeedDatabase.php
@@ -3,19 +3,39 @@
 namespace Glimmer\Tenancy\Jobs\TenantEvents;
 
 use Glimmer\Tenancy\Jobs\Concerns\TenantEventQueue;
+use Glimmer\Tenancy\Models\Tenant;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\File;
+use RuntimeException;
+use Spatie\Multitenancy\Contracts\IsTenant;
 
 class SeedDatabase extends TenantEventQueue
 {
+    public string $seeder = 'Database\\Seeders\\tenant\\DatabaseSeeder';
+
+    public bool $customSeeder = false;
+
+    public function __construct(IsTenant|Tenant $tenant, ?string $seeder = null)
+    {
+        parent::__construct($tenant);
+
+        if ($seeder) {
+            $this->seeder = $seeder;
+            $this->customSeeder = true;
+        }
+    }
+
     public function handle(): void
     {
         $this->tenant->execute(function () {
-            $exists = File::exists(database_path('seeders/tenant/DatabaseSeeder.php'));
+            $exists = class_exists($this->seeder);
+
+            if ($this->customSeeder && ! $exists) {
+                throw new RuntimeException("Seeder class $this->seeder does not exist.");
+            }
 
             Artisan::call('db:seed', array_merge(
                 ['--force' => true],
-                $exists ? ['--class' => 'Database\\Seeders\\tenant\\DatabaseSeeder'] : []
+                $exists ? ['--class' => $this->seeder] : []
             ));
         });
     }
